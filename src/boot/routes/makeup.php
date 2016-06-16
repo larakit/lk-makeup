655b39410c423ea94b45c19cc0630a9405f1dd39
Alexey Berdnikov <aberdnikov@gmail.com>
2016-06-16 15:00:18 +0500

    fixed

--- src/boot/routes/makeup.php
+++ src/boot/routes/makeup.php
@@ -5,58 +5,53 @@
 //****************************************
 //  Главная
 //****************************************
-\Larakit\Route\Route::group('makeup')
+\Larakit\Route\Route::item('makeup')
     ->setBaseUrl('/makeup')
     ->setNamespace('Larakit\Makeup\Controller')
-    ->routeIndex()
-    ->put();
-
-\Larakit\Route\Route::group('makeup/download')
+    ->put()
+;
+\Larakit\Route\Route::item('makeup/download')
     ->setBaseUrl('/makeup/download')
     ->setNamespace('Larakit\Makeup\Controller')
-    ->routeIndex()
     ->setController('Makeup')
     ->setAction('download')
-    ->put();
-
-\Larakit\Route\Route::group('makeup/block')
+    ->put()
+;
+\Larakit\Route\Route::item('makeup/block')
     ->setBaseUrl('/makeup/block-{block}')
     ->setNamespace('Larakit\Makeup\Controller')
-    ->routeIndex()
     ->setController('Makeup')
     ->setAction('block')
-    ->put();
-
-\Larakit\Route\Route::group('makeup/page')
+    ->put()
+;
+\Larakit\Route\Route::item('makeup/page')
     ->setBaseUrl('/makeup/page-{page}')
     ->setNamespace('Larakit\Makeup\Controller')
-    ->routeIndex()
     ->setController('Makeup')
     ->setAction('page')
-    ->put();
+    ->put()
+;
 
-\Larakit\Route\Route::group('makeup/frame_block')
+\Larakit\Route\Route::item('makeup/frame_block')
     ->setBaseUrl('/makeup/frame-block-{block}')
     ->setNamespace('Larakit\Makeup\Controller')
-    ->routeIndex()
     ->setController('Makeup')
     ->setAction('frameBlock')
-    ->put();
-
-\Larakit\Route\Route::group('makeup/frame_page')
+    ->put()
+;
+\Larakit\Route\Route::item('makeup/frame_page')
     ->setBaseUrl('/makeup/frame-page-{page}')
     ->setNamespace('Larakit\Makeup\Controller')
-    ->routeIndex()
     ->setController('Makeup')
     ->setAction('framePage')
-    ->put();
+    ->put()
+;
 
-\Larakit\Route\Route::group('lorempixel')
+
+\Larakit\Route\Route::item('lorempixel')
     ->setBaseUrl('/!/lorempixel/{w}x{h}/{cat}/{number}.jpeg')
     ->setNamespace('Larakit\Makeup\Controller')
-    ->routeIndex()
     ->setController('Makeup')
     ->setAction('lorempixel')
-    ->put();
-
-
+    ->put()
+;
